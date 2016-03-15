6fa7f7bccb3f27e7e1e800e37980d49788db2331
AricBa <bajingyun@163.com>
2016-03-15 09:51:52 +0800

    commit

--- leetCodeTest/www/js/Add_Digits.js
+++ leetCodeTest/www/js/Add_Digits.js
@@ -6,25 +6,36 @@
        * @param {number} num
        * @return {number}
        */
-      var addDigits = function(num) {
-        var currentNum = 0;
+      //var addDigits = function(num) {
+      //  var currentNum = 0;
+      //
+      //  while( num >= 10 ){
+      //    currentNum += (num % 10);
+      //    num = (( num - num % 10 ) / 10 );
+      //  }
+      //  currentNum += num;
+      //  if(currentNum >= 10){
+      //    addDigits(currentNum);
+      //  }else{
+      //    return currentNum;
+      //  }
+      //};
 
-        while( num >= 10 ){
-          currentNum += (num % 10);
-          num = (( num - num % 10 ) / 10 );
+      var addDigits = function (number) {
+        var remainder = number % 10;
+        var sum = remainder;
+        if(number >= 10) {
+          var rest = Math.floor(number / 10);
+          sum += addDigits(rest);
         }
-        currentNum += num;
-        if(currentNum >= 10){
-          addDigits(currentNum);
-        }else{
-          return currentNum;
-        }
+        return sum;
       };
+
 
       //var addDigits = function(num){
       //  return ( num %9 === 0 && num !== 0) ? 9 :(num % 9);
       //};
 
-      addDigits(19);
+      console.log(addDigits(19));
     }])
 }());
