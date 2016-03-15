82c4ca8df4ea581b955d69dc01b67b41f37bd2ef
AricBa <bajingyun@163.com>
2016-03-15 17:58:46 +0800

    commit

--- leetCodeTest/www/js/Add_Digits.js
+++ leetCodeTest/www/js/Add_Digits.js
@@ -6,36 +6,49 @@
        * @param {number} num
        * @return {number}
        */
-      //var addDigits = function(num) {
-      //  var currentNum = 0;
-      //
-      //  while( num >= 10 ){
-      //    currentNum += (num % 10);
-      //    num = (( num - num % 10 ) / 10 );
-      //  }
-      //  currentNum += num;
-      //  if(currentNum >= 10){
-      //    addDigits(currentNum);
-      //  }else{
-      //    return currentNum;
-      //  }
-      //};
+      var addDigits = function(num) {
+        var currentNum = 0;
 
-      var addDigits = function (number) {
-        var remainder = number % 10;
-        var sum = remainder;
-        if(number >= 10) {
-          var rest = Math.floor(number / 10);
-          sum += addDigits(rest);
+        while( num >= 10 ){
+          currentNum += (num % 10);
+          num = (( num - num % 10 ) / 10 );
         }
-        return sum;
+        console.log(num);
+        console.log(currentNum);
+        currentNum += num;
+        if(currentNum >= 10){
+           return addDigits(currentNum);
+        }else{
+          return currentNum;
+        }
       };
+      
+      var addDigits = function(num) {
+        var remainder = 0;
+        while(num >= 10){
+          remainder += num % 10;
+          num = (num - num % 10) / 10 ;
+        }
 
+        remainder += num;
+        if(remainder >= 10){
+          return addDigits(remainder);
+        }
+        else{
+          return remainder;
+        }
+      };
+//  var addDigits = function(num) {
+//     return 1 + (num - 1) % 9;
+// }
+// var addDigits = function(num) {
+      // return num === 0 ? 0 : num - 9 * Math.floor((num - 1) / 9);
+// };
 
       //var addDigits = function(num){
       //  return ( num %9 === 0 && num !== 0) ? 9 :(num % 9);
       //};
 
-      console.log(addDigits(19));
+      console.log(addDigits(11165));
     }])
 }());
